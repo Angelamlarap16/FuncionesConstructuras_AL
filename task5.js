0a0a039816cdf4d56d24fc3e9b4827e56836ad4b
Angela María Lara <angelamaria@1696gmail.com>
2026-09-23 18:19:16 -0500

    task 5 refresh

--- task5.js
+++ task5.js
@@ -1,5 +1,15 @@
 // ¡Escribe tu código debajo de esta línea!
+function FriendsList(arregloNombres){
+    this.nombres = arregloNombres;
+}
 
+const cantidad = process.argv[3];
 
+let listaNombres = []
+    for (let i = 4; i < process.argv.length; i++){
+        listaNombres.push(process.argv[i])
+    }
 
+const misAmigos = new FriendsList(listaNombres)
+console.log(misAmigos.nombres)
 // ¡Escribe tu código encima de esta línea!
